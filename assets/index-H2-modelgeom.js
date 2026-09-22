0e782f061f3f997b9ca36d72b1d38f0008def478
orrimgames <orrimgames@tuta.io>
2026-09-22 14:22:31 -0500

    feed clearance slider min lowered to 0.01 m (net-tape skim) per user request

--- assets/index-H2-modelgeom.js
+++ assets/index-H2-modelgeom.js
@@ -30220,7 +30220,7 @@ async function ZE() {
         wrap.id = "feedctl";
         wrap.innerHTML = '<button id="feedtoggle" type="button">FEED &#9662;</button>' +
           '<div id="feedpanel">' +
-          '<label>Net clearance <span id="fv_clear"></span></label><input id="fs_clear" type="range" min="0.5" max="8" step="0.25" value="2.5">' +
+          '<label>Net clearance <span id="fv_clear"></span></label><input id="fs_clear" type="range" min="0.01" max="8" step="0.01" value="2.5">' +
           '<label>Spin <span id="fv_spin"></span></label><input id="fs_spin" type="range" min="-4000" max="4000" step="250" value="3000">' +
           '<label>Speed <span id="fv_speed"></span> <button id="fs_auto" type="button">AUTO</button></label><input id="fs_speed" type="range" min="12" max="62.6" step="0.5" value="23">' +
           '</div>';
